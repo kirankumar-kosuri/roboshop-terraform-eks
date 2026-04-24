26501a974bf883aa4c5e6d45a0890a76e89419c4
kirankumarkosuri7 <kirankumarkosuri86@gmail.com>
2026-04-24 16:29:29 +0530

    vpc

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00183562CZ75AW6T2J1V"
+}
+
+variable "domain_name" {
+    default = "daws86s.blog"
+}
